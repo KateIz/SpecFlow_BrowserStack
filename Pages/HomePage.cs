61ad31abedf0873986a866d8ba0c28c7c9b92e46
Kateryna Izviekova <katizvekova@gmail.com>
2022-03-27 15:00:08 +0200

    Add project files.

---
+++ Pages/HomePage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
+
+
+namespace Pages
+{
+   [Binding]
+    public class HomePage
+    {
+        IWebDriver driver;
+
+
+        public bool OpenHomePage()
+            {
+                driver = new ChromeDriver();
+                string url = "https://www.bbc.com/";
+                driver.Navigate().GoToUrl(url);
+                return true;
+            }
+
+            public bool ClickAcceptCookies()
+            {
+                IWebElement button = driver.FindElement(By.XPath("//p[contains(text(),'Consent')]"));
+                if (button.Displayed)
+                {
+                    button.Click();
+                    Console.WriteLine("Udalo sie!!! xD");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("nie udalo sie!!! xD");
+                    return false;
+                }
+            }
+
+            public bool VerifyUrl()
+            {
+                String page_url = driver.Url;
+                string url = "www.bbc.com";
+
+                bool isValid;
+                if (page_url.Contains(url))
+                {
+                    isValid = true;
+                    Console.WriteLine("Udało się:");
+                }
+                else
+                {
+                    isValid = false;
+                    Console.WriteLine("Nie udało się:");
+                }
+                return isValid;
+            }
+            public bool ClickSignIn()
+            {
+              IWebElement button = driver.FindElement(By.Id("idcta-link"));
+                if (button.Displayed)
+                {
+                    button.Click();
+                    Console.WriteLine("Udalo sie!!! xD");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("nie udalo sie!!! xD");
+                    return false;
+                }
+                
+                return true;
+
+            }
+        
+            public void ClickRegisterNow()
+        {                                                             
+            IWebElement BtnRegisterNow = driver.FindElement(By.XPath("//a[contains(@class,'link--primary')]"));    
+            BtnRegisterNow.Click();
+        }
+    }
+}
---
+++ Steps/HomePageSteps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using NUnit.Framework;
+using Pages;
+
+namespace Steps
+{
+    [Binding]
+    public sealed class HomePageSteps
+    {
+
+        HomePage home_page = new HomePage();
+        [Given(@"Go to page")]
+        public void GivenGoToPage()
+        {
+            Assert.IsTrue(home_page.OpenHomePage());
+        }
+
+        [When(@"Accept coockies")]
+        public void AcceptCoockies()
+        {
+            Assert.IsTrue(home_page.ClickAcceptCookies());
+        }
+
+        [Then(@"I should see BBC portal")]
+        public void ThenIShouldSeeBBCPortal()
+        {
+            Assert.IsTrue(home_page.VerifyUrl());
+        }
+
+        [When(@"I click Sign In")]
+        public void WhenIClickSignIn()
+        {
+          Assert.IsTrue(home_page.ClickSignIn());
+        }
+
+        [When(@"I click Register Now")]
+        public void WhenIClickRegisterNow()
+        {
+            home_page.ClickRegisterNow();
+        }
+    }
+}
